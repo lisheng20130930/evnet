13fb2a535044f19b32d3aa84d2ac80ceba2dc8c5
Listen.Li <listioning@163.com>
2015-05-29 10:49:32 +0800

    Create dataqueue.h

---
+++ dataqueue.h
@@ -0,0 +1,35 @@
+#ifndef DATAQUEUE_H
+#define DATAQUEUE_H
+
+
+#include "stdlib.h"
+#include "memory.h"
+
+
+#ifndef __max
+#define __max(a,b)  (((a) > (b)) ? (a) : (b))
+#endif
+
+
+typedef struct dataqueue_s
+{    
+    unsigned int insertPos;
+    unsigned int terminalPos;
+    unsigned int queryPos;
+    unsigned int dataSize;
+    unsigned int bufferSize;
+    char *m_pDataQueueBuffer;
+}dataqueue_t;
+
+
+#define dataqueue_init(p)  do{memset(p, 0x00, sizeof(dataqueue_t));}while(0);
+#define dataqueue_uinit(p) dataqueue_remove_data(p,1)
+#define dataqueue_datasize(p) ((p)->dataSize)
+
+void dataqueue_remove_data(dataqueue_t *queue, int bFreeMemroy);
+void dataqueue_insert_data(dataqueue_t *queue, void *pBuffer, int wDataSize);
+void dataqueue_distill_data(dataqueue_t *queue, int wDataSize);
+void dataqueue_next_data(dataqueue_t *queue, char **ppData, int *pLen);
+
+
+#endif
